6c0f393e4c8e63d4b13bf433ba31070c514a45c8
KAIJIE Zhan <ceres0503@icloud.com>
2017-03-22 18:30:41 -0400

    update

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import {Recipe} from '../recipe';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'rb-recipe-list',
-  templateUrl: './recipe-list.component.html'
+  templateUrl: 'recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
-  recipe = new Recipe('Dummy', 'Dummy', 'https://media1.giphy.com/media/Kw81cekcrRw88/200_s.gif');
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Very tasty', 'https://upload.wikimedia.org/wikipedia/commons/' +
+      'thumb/a/ae/Wiener-Schnitzel02.jpg/1200px-Wiener-Schnitzel02.jpg', []),
+    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/' +
+      'the_perfect_summer_salad.jpg', [])
+  ];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
+  constructor() {}
 
   ngOnInit() {
+  }
+
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
   }
 
 }
